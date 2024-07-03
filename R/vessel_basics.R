88ba18ac6faf7b820fbb16d4313eed737a3d1745
ryjohnson09 <johnsrc06@gmail.com>
2024-07-03 11:03:14 -0400

    add vessel_basics

---
+++ R/vessel_basics.R
@@ -0,0 +1,28 @@
+#' Vessel Basics
+#'
+#' @description
+#' A wrapper to extract various basic information about a specific vessel.
+#'
+#' @param VesselID Numeric ID for a specific vessel. If blank, all vessel information will be returned.
+#' @param apiaccesscode WSDOT Access Code.
+#'
+#' @return Vessel basic information organized as a list.
+#' @export
+#'
+#' @examples
+#' vessel_basics(38)
+vessel_basics <- function(VesselID = NULL, apiaccesscode = Sys.getenv("WSDOT_ACCESS_CODE")) {
+
+  # If VesselID is provided
+  if (is.null(VesselID)) {
+    endpoint = "http://www.wsdot.wa.gov/Ferries/API/Vessels/rest/vesselbasics"
+  } else {
+    endpoint = paste0("http://www.wsdot.wa.gov/Ferries/API/Vessels/rest/vesselbasics", "/", VesselID)
+  }
+
+  # Make Request
+  httr2::request(endpoint) |>
+    httr2::req_url_query(apiaccesscode = apiaccesscode) |>
+    httr2::req_perform() |>
+    httr2::resp_body_json()
+}
